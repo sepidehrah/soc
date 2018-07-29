7ad939b77d378dca0b60c62c624b2342b90714f0
sepidehrah <rahimian.sepideh1377@yahoo.com>
2018-07-29 11:45:18 +0430

    complete search

--- search.py
+++ search.py
@@ -11,20 +11,20 @@ def login():
     elif request.method == "POST":
         if request.form["uname"] != "admin" or request.form["password"] != "1234":
             error = "Invalid Credentials. Please try again."
+        	# return render_template("login.html", error = error)
             return redirect(url_for("login"))
         else:
             return redirect(url_for("search"))
-        return render_template("login.html", error = error)
+        # return render_template("login.html", error = error)
 
 @app.route("/main", methods = ["GET", "POST"])
 def search():
 	if request.method == "GET":
 		return render_template("main.html")
 	elif request.method == "POST":
-		return get_information(request.form["search_word"])
+		information = get_information(request.form["search_word"])
+		return render_template("main.html", information = information)
 	
 def get_information(search_word):
-	req = requests.get("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=" + search_word)
-	page_id = list(req.json()['query']['pages'].keys())[0]
-	print('THIS IS PAGE ID', page_id)
-	return req.json()['query']['pages'][page_id]['extract']
+	req = requests.get("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&exsentences=2&explaintext=&redirects=&format=json&formatversion=2&titles=" + search_word)
+	return req.json()['query']['pages'][0]['extract']
