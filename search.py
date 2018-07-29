50ecb5ca589ccc396099fc295a27817c6e9efec5
sepidehrah <rahimian.sepideh1377@yahoo.com>
2018-07-29 13:30:48 +0430

    add session part

--- search.py
+++ search.py
@@ -1,7 +1,11 @@
 import requests
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, session
 app = Flask(__name__)
 
+# if __name__ == "__main__":
+app.secret_key = 'some secret key'
+    # app.debug = True
+    # app.run()
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
@@ -11,19 +15,22 @@ def login():
     elif request.method == "POST":
         if request.form["uname"] != "admin" or request.form["password"] != "1234":
             error = "Invalid Credentials. Please try again."
-        	# return render_template("login.html", error = error)
-            return redirect(url_for("login"))
+            return render_template("login.html", error = error)
+            # return redirect(url_for("login"))
         else:
+            session['username'] =  request.form["uname"]
             return redirect(url_for("search"))
         # return render_template("login.html", error = error)
 
 @app.route("/main", methods = ["GET", "POST"])
 def search():
-	if request.method == "GET":
-		return render_template("main.html")
-	elif request.method == "POST":
-		information = get_information(request.form["search_word"])
-		return render_template("main.html", information = information)
+	if len(session) != 0:
+		if request.method == "GET":
+			return render_template("main.html")
+		elif request.method == "POST":
+			information = get_information(request.form["search_word"])
+			return render_template("main.html", information = information)
+	return redirect(url_for("login"))
 	
 def get_information(search_word):
 	req = requests.get("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&exsentences=2&explaintext=&redirects=&format=json&formatversion=2&titles=" + search_word)
